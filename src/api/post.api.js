54e2fa794603a6c0e3808055f3886a6b38cf802d
namnhGru <namnh@infinigru.com>
2020-05-13 14:52:33 +0700

    push to heroku

--- src/api/post.api.js
+++ src/api/post.api.js
@@ -1,9 +1,11 @@
 import PostModel from "../model/post.model";
+import TagModel from "../model/tag.model";
 
 export async function getAllPosts(req, res) {
   try {
-    const posts = await PostModel.find();
-    console.log(posts);
+    const posts = await PostModel.find()
+      .populate({ path: "tags", model: TagModel })
+      .exec();
     res.status(200).json({ data: posts });
   } catch (err) {
     res.status(400).json({ err: err.errmsg });
